fe7da4130127f3a34490fa443dd2e099aaad81c4
JosefForkman <strand.vatten@outlook.com>
2023-08-24 16:02:48 +0100

    big push to last section of index

--- main.js
+++ main.js
@@ -1,26 +1,87 @@
 const steppers = document.querySelectorAll(".stepper-item");
-const stepperActive = document.querySelector(".stepper-item.active");
-const steppersCompleted = document.querySelectorAll(".stepper-item.completed");
 
-steppers.forEach((stepper) => {
+const gameFrame = document.querySelector("iframe");
+const backBtn = document.querySelector(".back-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+const time = document.querySelector(".time");
+let stopwatchId = stopwatch()
+
+const demoGame = [
+    "https://scratch.mit.edu/projects/884792694/embed",
+    "https://scratch.mit.edu/projects/884735449/embed",
+    "https://scratch.mit.edu/projects/884464291/embed",
+];
+
+let currentSelectedStepper = 0;
+
+steppers.forEach((stepper) =>
     stepper.addEventListener("click", (event) => {
-        const curentStepper = event.currentTarget;
+        selectStepper(event.currentTarget);
+        setGameIframe();
+    })
+);
 
-        /* check if click on active stepper */
-        // console.log(curentStepper == stepperActive);
-        // console.log(curentStepper.classList.contains('active'));
+function selectStepper(currentStepper) {
+    steppers.forEach((stepper) => {
+        stepper.classList = "stepper-item";
+        currentStepper.classList.add("active");
 
-        steppers.forEach(stepper => {
-            console.log(stepper.classList.contains('active'));
-            console.log(curentStepper.dataset.stepp);
+        if (
+            stepper.dataset.stepp < currentStepper.dataset.stepp ||
+            steppers[steppers.length - 1].dataset.stepp ==
+                currentStepper.dataset.stepp
+        ) {
+            stepper.classList = "stepper-item completed";
+        }
 
-            if (stepper.dataset.stepp < curentStepper.dataset.stepp) {
-                stepper.classList = "stepper-item completed"
-            } else {
-                stepper.classList = "stepper-item"
-            }
-
-            curentStepper.classList.add("active")
-        })
+        currentSelectedStepper = Number.parseInt(currentStepper.dataset.stepp);
     });
+}
+
+backBtn.addEventListener("click", () => {
+    if (currentSelectedStepper != 0) {
+        currentSelectedStepper = currentSelectedStepper - 1;
+        selectStepper(steppers[currentSelectedStepper]);
+        setGameIframe();
+    }
 });
+nextBtn.addEventListener("click", () => {
+    if (currentSelectedStepper != steppers.length - 1) {
+        currentSelectedStepper = currentSelectedStepper + 1;
+        selectStepper(steppers[currentSelectedStepper]);
+        setGameIframe();
+    }
+});
+
+function setGameIframe() {
+    gameFrame.src = demoGame[0];
+    if (demoGame.length >= currentSelectedStepper) {
+        gameFrame.src = demoGame[currentSelectedStepper];
+    }
+    
+    /* Check if on last stepper */
+    if (currentSelectedStepper == demoGame.length - 1) {
+        clearInterval(stopwatchId)
+    }
+}
+function stopwatch() {
+    const countDate = new Date().setTime(new Date().getTime() + 1000 * 60);
+
+    return setInterval(() => {
+        const now = new Date().getTime();
+        const gap = countDate - now;
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+
+        const textMinute = Math.floor((gap % hour) / minute);
+        const textSecond = Math.floor((gap % minute) / second);
+
+        if (gap >= 0) {
+            time.textContent = `${textMinute}:${textSecond}`;
+        }
+    }, 1000);
+}
+
